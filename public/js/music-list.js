61051224ee5f083bad76a9362b608f0368d5dd06
Luciano Benedetti <84484798+Benedetti21@users.noreply.github.com>
2022-01-27 11:12:31 -0300

    Update music-list.js

--- public/js/music-list.js
+++ public/js/music-list.js
@@ -1,19 +1,8 @@
-// To add more song, just copy the following code and paste inside the array
-
-//   {
-//     name: "Here is the music name",
-//     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-//   }
-
-//paste it inside the array as more as you want music then you don't need to do any other thing
-
 let allMusic = [
     {
       name: "Sugar Were Going Down",
       artist: "Fall Out Boy",
-      img: "music-1",
+      img: "fob-SugarWereGoingDown",
       src: "fob-SugarWereGoingDown"
     },
     {
@@ -25,46 +14,39 @@ let allMusic = [
     {
       name: "Ghost",
       artist: "Justin Bieber",
-      img: "music-3",
+      img: "justinBieber-Ghost",
       src: "justinBieber-Ghost"
     },
     {
       name: "Two Ghosts",
       artist: "Harry Styles",
-      img: "music-4",
+      img: "harryStyles-TwoGhosts",
       src: "harryStyles-TwoGhosts"
     },
     {
       name: "Somewhere Only We Know",
       artist: "Keane",
-      img: "music-5",
+      img: "keane-SomewhereOnlyWeKnow",
       src: "keane-SomewhereOnlyWeKnow"
     },
     {
       name: "The Only Exception",
       artist: "Paramore",
-      img: "music-6",
+      img: "paramore-TheOnlyException",
       src: "paramore-TheOnlyException"
     },
     {
       name: "Over My Head Cable Car",
       artist: "The Fray",
-      img: "music-1",
+      img: "theFray-OverMyHeadCableCar",
       src: "theFray-OverMyHeadCableCar"
     },
     {
       name: "In Your Eyes",
       artist: "The Weeknd",
-      img: "music-1",
+      img: "theWeeknd-InYourEyes",
       src: "theWeeknd-InYourEyes"
     }
     
-
-    // like this paste it and remember to give comma after ending of this bracket }
-    // {
-    //   name: "Here is the music name",
-    //   artist: "Here is the artist name",
-    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-    // }
+    
   ];
